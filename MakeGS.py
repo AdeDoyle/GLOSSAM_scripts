2169eac6a515e7331ff28db6b60942c5fc2d6261
Adrian <admd911@hotmail.com>
2024-11-27 11:32:14 +0000

    MakeGS.py updated to include function to extract gloss content, and Lemma data refined.

--- MakeGS.py
+++ MakeGS.py
@@ -32,8 +32,43 @@ def get_xml_lemmata(file_name):
         sent_list.append(sentence)
         text_reduce = text_reduce[text_reduce.find("</ab>") + len("</ab>"):]
 
+    # Refine the sentence list to a list of lemmata
+    lem_list = list()
     for sent in sent_list:
-        print([sent])
+        if '<seg type="lemma"' in sent:
+            for _ in range(sent.count('<seg type="lemma"')):
+                sent = sent[sent.find('<seg type="lemma"') + len('<seg type="lemma"'):]
+                id_no = sent[sent.find("xml:id=") + len("xml:id="):sent.find(">")]
+                id_no = id_no.strip()
+                sent = sent[sent.find(">") + 1:]
+                lemma = sent[:sent.find("</seg>")]
+                sent = sent[sent.find("</seg>") + len("</seg>"):]
+                lem_list.append((id_no, lemma))
+
+    for lem in lem_list:
+        print(lem)
+
+    return ""
+
+
+def get_xml_glosses(file_name):
+
+    # Load xml file
+    gold_xml = open_xml(file_name)
+
+    # Isolate relevant contents
+    text_range = gold_xml[gold_xml.find("<hi:listGlossGrp>"):gold_xml.find("</hi:listGlossGrp>") + len("</hi:listGlossGrp>")]
+
+    # Add each gloss grouping to a list of gloss groups
+    group_list = list()
+    text_reduce = text_range[:]
+    for _ in range(text_range.count("</hi:glossGrp>")):
+        gloss_group = text_reduce[text_reduce.find("<hi:glossGrp "):text_reduce.find("</hi:glossGrp>") + len("</hi:glossGrp>")]
+        group_list.append(gloss_group)
+        text_reduce = text_reduce[text_reduce.find("</hi:glossGrp>") + len("</hi:glossGrp>"):]
+
+    # for grp in group_list:
+    #     print([grp])
 
     return ""
 
@@ -45,10 +80,10 @@ def gen_gs():
     :return:
     """
 
-    # Load xml file
-    gold_xml = get_xml_lemmata("Isidore_Gold")
+    gold_xml_lemmata = get_xml_lemmata("Isidore_Gold")
+    gold_gloss_data = get_xml_glosses("Isidore_Gold")
 
-    return gold_xml
+    return gold_gloss_data
 
 # Create an algorithm to produce gold standard for glossing relationships, based on Evina’s data.
 # First extract all gloss info (glosses, lemmata, IDs).
