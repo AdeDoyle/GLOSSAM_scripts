f55b86a9f7744ccc9598f3c4bfd57e980db23015
admd9 <admd911@hotmail.com>
2025-03-01 22:26:43 +0000

    TextSim.py updated to detect relationships between glosses using LLM method, and to plot on 2D graph.

--- TextSim.py
+++ TextSim.py
@@ -1,5 +1,12 @@
 from nltk import edit_distance as ed
 from MakeGS import load_gs
+import pandas as pd
+from sklearn.manifold import TSNE
+import plotly.express as px
+from sklearn.cluster import KMeans, DBSCAN
+import torch
+from sentence_transformers import SentenceTransformer
+import pickle as pkl
 
 
 def norm_ld(s1, s2):
@@ -49,7 +56,7 @@ def lcs(s1, s2):
 
 
 def slcs(s1, s2):
-    """finds the longest, then the second longest common substring of two strings"""
+    """finds the longest, then the second-longest common substring of two strings"""
 
     lcsubstr = lcs(s1, s2)
 
@@ -82,9 +89,70 @@ def lcs_compare(s1, s2, n=82):
     return result
 
 
-def compare_glosses(glosses, method):
+def apply_clustering(emb_array, clustering_method="DBSCAN", clusters=2):
+
+    embedded_clusters = None
+
+    if clustering_method == "KMeans":
+        kmeans = KMeans(n_clusters=clusters, random_state=42)
+        embedded_clusters = kmeans.fit_predict(emb_array)
+
+    elif clustering_method == "DBSCAN":
+        dbscan = DBSCAN(eps=1.5, min_samples=2)
+        embedded_clusters = dbscan.fit_predict(emb_array)
+
+    return embedded_clusters
+
+
+def plot_clusters(sentences, embeddings, clustered_embeddings, plot_name="Latin Gloss Embeddings"):
+
+    tsne_embedded = TSNE(n_components=2).fit_transform(embeddings)
+
+    df_embeddings = pd.DataFrame(tsne_embedded)
+    df_embeddings = df_embeddings.rename(columns={0: 'x', 1: 'y'})
+    df_embeddings = df_embeddings.assign(label=clustered_embeddings)
+    df_embeddings = df_embeddings.assign(text=sentences)
+
+    fig = px.scatter(
+        df_embeddings, x='x', y='y',
+        color='label', labels={'color': 'label'},
+        hover_data=['text'], title=plot_name
+    )
+
+    return fig
+
+
+def llm_compare(gloss_1, gloss_2, gloss_vec_mapping, model, n=50):
+
+    similarity_score = model.similarity(gloss_vec_mapping.get(gloss_1), gloss_vec_mapping.get(gloss_2))
+    similarity_score = similarity_score.item()
+
+    cutoff = n / 100
+
+    if similarity_score >= cutoff:
+        result = "Related"
+    else:
+        result = "Unrelated"
+
+    return result
+
+
+def save_cluster_plot(fig, file_name="Scatter Plot.pkl"):
+    with open(file_name, 'wb') as spl:
+        pkl.dump(fig, spl)
+
+
+def load_cluster_plot(file_name="Scatter Plot.pkl"):
+    with open(file_name, 'rb') as loadfile:
+        file_loaded = pkl.load(loadfile)
+
+    return file_loaded
+
+
+def compare_glosses(glosses, method, gloss_vec_mapping=None, model=None):
     """
     Compares two sets of glosses, predicts whether each gloss is related or unrelated
+    Scores predictions by comparison to known correct labels to calculate Accuracy, precision, recall and f-measure
     """
 
     labels = [g[2] for g in glosses]
@@ -92,9 +160,13 @@ def compare_glosses(glosses, method):
     # If using the edit distance method
     if method == "ED":
         results = [ed_compare(g[0], g[1]) for g in glosses]
+    # If using the longest common substring method
     elif method == "LCS":
         results = [lcs_compare(g[0], g[1]) for g in glosses]
-    # If using any other method (none ready yet)
+    # If using a large language model method
+    elif method == "LLM":
+        results = [llm_compare(g[0], g[1], gloss_vec_mapping, model) for g in glosses]
+    # If using any other method
     else:
         results = labels
 
@@ -131,8 +203,43 @@ def compare_glosses(glosses, method):
 if __name__ == "__main__":
 
     dev_set = load_gs("Gold Standard Dev.pkl")
-    print(compare_glosses(dev_set, "ED"))
-    print(compare_glosses(dev_set, "LCS"))
+
+    # print(compare_glosses(dev_set, "ED"))
+    # print(compare_glosses(dev_set, "LCS"))
+
+    # # Test CUDA and GPU
+    # print(torch.cuda.is_available())
+    # print(torch.cuda.get_device_name(0))
+
+    # Select text to embed
+    glosses_to_embed = sorted(list(set(
+        [g[0] for g in dev_set] + [g[1] for g in dev_set]
+    )))
+
+    # Identify models
+    m1 = "silencesys/paraphrase-xlm-r-multilingual-v1-fine-tuned-for-medieval-latin"
+    m2 = "silencesys/paraphrase-xlm-r-multilingual-v1-fine-tuned-for-latin"
+
+    llm = SentenceTransformer(m1)
+
+    embedded_sentences = llm.encode(glosses_to_embed)
+    gloss_dict = dict()
+    for gloss_index, gloss in enumerate(glosses_to_embed):
+        gloss_dict[gloss] = embedded_sentences[gloss_index]
+    clusters = apply_clustering(embedded_sentences, "KMeans", 6)
+
+    print(compare_glosses(dev_set, "LLM", gloss_dict, llm))
+
+    # Create a plot from the development set
+    plot = plot_clusters(glosses_to_embed, embedded_sentences, clusters)
+    plot.show()
+
+    # # Save the plot as a pickle file
+    # save_cluster_plot(plot)
+
+    # # Load the pickle file for the plot and display it
+    # loaded_matrix = load_cluster_plot()
+    # loaded_matrix.show()
 
     """
     The following results were recorded using the edit distance method to compare glosses from the development set.
@@ -226,3 +333,121 @@ if __name__ == "__main__":
         Recall: 0.0,
         f-measure: 0
     """
+
+    """
+        The following results were recorded using the lowest common substring method:
+
+        n = 82
+            TP: 661, FP: 85, TN: 1181, FN: 109
+            Accuracy: 0.9,
+            Precision: 0.89,
+            Recall: 0.86,
+            f-measure: 0.87
+    """
+
+    """
+        The following results were recorded using the LLM method:
+
+        n = 0
+            TP: 770, FP: 1202, TN: 64, FN: 0
+            Accuracy: 0.41,
+            Precision: 0.39,
+            Recall: 1.0,
+            f-measure: 0.56
+        n = 10
+            TP: 767, FP: 1008, TN: 258, FN: 3
+            Accuracy: 0.5,
+            Precision: 0.43,
+            Recall: 1.0,
+            f-measure: 0.6
+        n = 20
+            TP: 756, FP: 652, TN: 614, FN: 14
+            Accuracy: 0.67,
+            Precision: 0.54,
+            Recall: 0.98,
+            f-measure: 0.7
+        n = 30
+            TP: 735, FP: 367, TN: 899, FN: 35
+            Accuracy: 0.8,
+            Precision: 0.67,
+            Recall: 0.95,
+            f-measure: 0.79
+        n = 40
+            TP: 712, FP: 184, TN: 1082, FN: 58
+            Accuracy: 0.88,
+            Precision: 0.79,
+            Recall: 0.92,
+            f-measure: 0.85
+        n = 43
+            TP: 706, FP: 145, TN: 1121, FN: 64
+            Accuracy: 0.9,
+            Precision: 0.83,
+            Recall: 0.92,
+            f-measure: 0.87
+        n = 45
+            TP: 690, FP: 126, TN: 1140, FN: 80
+            Accuracy: 0.9,
+            Precision: 0.85,
+            Recall: 0.9,
+            f-measure: 0.87
+        n = 47
+            TP: 677, FP: 112, TN: 1154, FN: 93
+            Accuracy: 0.9,
+            Precision: 0.86,
+            Recall: 0.88,
+            f-measure: 0.87
+        n = 50
+            TP: 662, FP: 100, TN: 1166, FN: 108
+            Accuracy: 0.9,
+            Precision: 0.87,
+            Recall: 0.86,
+            f-measure: 0.86
+        n = 53
+            TP: 640, FP: 84, TN: 1182, FN: 130
+            Accuracy: 0.89,
+            Precision: 0.88,
+            Recall: 0.83,
+            f-measure: 0.85
+        n = 55
+            TP: 633, FP: 69, TN: 1197, FN: 137
+            Accuracy: 0.9,
+            Precision: 0.9,
+            Recall: 0.82,
+            f-measure: 0.86
+        n = 57
+            TP: 623, FP: 63, TN: 1203, FN: 147
+            Accuracy: 0.9,
+            Precision: 0.91,
+            Recall: 0.81,
+            f-measure: 0.86
+        n = 60
+            TP: 603, FP: 51, TN: 1215, FN: 167
+            Accuracy: 0.89,
+            Precision: 0.92,
+            Recall: 0.78,
+            f-measure: 0.84
+        n = 70
+            TP: 542, FP: 28, TN: 1238, FN: 228
+            Accuracy: 0.87,
+            Precision: 0.95,
+            Recall: 0.7,
+            f-measure: 0.81
+        n = 80
+            TP: 404, FP: 12, TN: 1254, FN: 366
+            Accuracy: 0.81,
+            Precision: 0.97,
+            Recall: 0.52,
+            f-measure: 0.68
+        n = 90
+            TP: 329, FP: 7, TN: 1259, FN: 441
+            Accuracy: 0.78,
+            Precision: 0.98,
+            Recall: 0.43,
+            f-measure: 0.6
+        n = 100
+            TP: 166, FP: 3, TN: 1263, FN: 604
+            Accuracy: 0.7,
+            Precision: 0.98,
+            Recall: 0.22,
+            f-measure: 0.36
+        """
