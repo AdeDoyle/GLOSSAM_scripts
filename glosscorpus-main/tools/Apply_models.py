8cde1125dc1d0708617bdf5fbaa3bfb56b6cf31c
admd9 <admd911@hotmail.com>
2025-06-23 00:59:44 +0100

    Apply_models.py updated so that all functions to apply gloss comparison models to texts adhere to the new structure of the Gloss Corpus website.

--- glosscorpus-main/tools/Apply_models.py
+++ glosscorpus-main/tools/Apply_models.py
@@ -6,161 +6,189 @@ from sentence_transformers import SentenceTransformer
 import pandas as pd
 
 
-def prep_files(folder_path="default", normalise=False):
-    """Prepare TEI encoded project files for comparison"""
+def prep_files(normalise=False):
+    """Prepare contents of TEI encoded gloss collection files for comparison using models"""
 
-    # Get filenames
-    if folder_path == "default":
-        curdir = os.getcwd()
-        folder_path = os.path.join(curdir, "gloss_comparisons")
-    filenames = os.listdir(folder_path)
+    # Map Directories
+    tools_dir = os.getcwd()
+    tools_parent = os.path.dirname(tools_dir)
+    texts_dir = os.path.join(tools_parent, "data", "texts")
 
-    # Load file data
-    filetexts = list()
-    for filename in filenames:
-        if filename.startswith('.'):
-            continue
-        with open(os.path.join(folder_path, filename), 'r', encoding="utf-8") as loadfile:
-            file_loaded = loadfile.read()
-        filetexts.append(file_loaded[file_loaded.find("<text>"):file_loaded.find("</text>") + len("</text>")])
+    # Create lists to collect directories for each base text and associated gloss collections
+    base_texts = os.listdir(texts_dir)
+    gloss_collections = list()
 
-    # Split file data into lists of data for individual glosses
-    glosses = list()
-    for filetext in filetexts:
-        filetext = filetext[filetext.find("<note "):filetext.rfind("</note>") + len("</note>")]
-        # Remove notes
-        filetext_split = filetext.split("<!--")
-        filetext_split = [i if "-->" not in i else i[i.find("-->") + 3:] for i in filetext_split]
-        filetext = "".join(filetext_split)
-        # Tidy up html
-        file_glosses = ["<note n=" + i[:i.rfind("</note>") + len("</note>")] for i in filetext.split("<note n=") if i]
-        file_glosses = [" ".join(i.split("\n")) for i in file_glosses]
-        file_glosses = [" ".join(i.split("\t")) for i in file_glosses]
-        for i_indx, i in enumerate(file_glosses):
-            while "  " in i:
-                i = " ".join(i.split("  "))
-            file_glosses[i_indx] = i.strip()
-        glosses.append(file_glosses)
+    for text in base_texts:
+        text_path = os.path.join(texts_dir, text)
+        collections_path = os.path.join(text_path, "gloss_collections")
+        for gloss_coll in os.listdir(collections_path):
+            gloss_collections.append(os.path.join(collections_path, gloss_coll))
 
-    # Refine the data for individual glosses
-    glosses_data = list()
-    for collection_index, gloss_collection in enumerate(glosses):
-        this_file = filenames[collection_index].strip(".xml")
-        for gloss_html in gloss_collection:
-            gloss_soup = BeautifulSoup(gloss_html, 'html.parser')
-            gloss_id = gloss_soup.find('note')['n']
-            lemma_id = gloss_soup.find('note')['target']
-            try:
-                gloss_text = gloss_soup.find("gloss").text
-            except AttributeError:
+    # Get gloss collection data for each base text, and add each separately to a dictionary
+    texts_dict = dict()
+    for text in base_texts:
+        filetexts = list()
+        for filename in gloss_collections:
+            # Load files and add text (between text tags) for each collection to a list
+            if filename.startswith('.'):
                 continue
-            glosses_data.append([this_file, gloss_id, lemma_id, gloss_text])
-    if normalise:
-        glosses_data = [[gd[0], gd[1], gd[2], gd[3].lower()] for gd in glosses_data]
-        glosses_data = [[gd[0], gd[1], gd[2], "u".join(gd[3].split("v"))] for gd in glosses_data]
-        glosses_data = [[gd[0], gd[1], gd[2], "i".join(gd[3].split("j"))] for gd in glosses_data]
-        for punct in [p for p in string.punctuation + "«»"]:
-            glosses_data = [[gd[0], gd[1], gd[2], "".join(gd[3].split(punct))] for gd in glosses_data]
-        glosses_data = [[gd[0], gd[1], gd[2], " ".join(gd[3].split("  "))] for gd in glosses_data]
+            elif text in filename:
+                with open(filename, 'r', encoding="utf-8") as loadfile:
+                    file_loaded = loadfile.read()
+                filetexts.append(file_loaded[file_loaded.find("<text>"):file_loaded.find("</text>") + len("</text>")])
 
-    # Pair glosses on the same lemma from different manuscripts, and remove all unpaired glosses
-    gloss_pairs = list()
-    for gl_index, gloss_datum in enumerate(glosses_data):
-        for remaining_gloss in glosses_data[gl_index:]:
-            if gloss_datum[0] != remaining_gloss[0] and gloss_datum[2] == remaining_gloss[2]:
-                gloss_pairs.append([gloss_datum, remaining_gloss])
+        # Split each collection file data into lists of data for individual glosses
+        glosses = list()
+        for filetext in filetexts:
+            filetext = filetext[filetext.find("<note "):filetext.rfind("</note>") + len("</note>")]
+            # Remove notes
+            filetext_split = filetext.split("<!--")
+            filetext_split = [i if "-->" not in i else i[i.find("-->") + 3:] for i in filetext_split]
+            filetext = "".join(filetext_split)
+            # Tidy up html
+            file_glosses = [
+                "<note n=" + i[:i.rfind("</note>") + len("</note>")] for i in filetext.split("<note n=") if i
+            ]
+            file_glosses = [" ".join(i.split("\n")) for i in file_glosses]
+            file_glosses = [" ".join(i.split("\t")) for i in file_glosses]
+            for i_indx, i in enumerate(file_glosses):
+                while "  " in i:
+                    i = " ".join(i.split("  "))
+                file_glosses[i_indx] = i.strip()
+            glosses.append(file_glosses)
 
-    prepped = gloss_pairs
+        # Refine the data for individual glosses
+        glosses_data = list()
+        for collection_index, gloss_collection in enumerate(glosses):
+            this_file = os.path.basename(gloss_collections[collection_index]).strip(".xml")
+            for gloss_html in gloss_collection:
+                gloss_soup = BeautifulSoup(gloss_html, 'html.parser')
+                gloss_id = gloss_soup.find('note')['n']
+                lemma_id = gloss_soup.find('note')['target']
+                try:
+                    gloss_text = gloss_soup.find("gloss").text
+                except AttributeError:
+                    continue
+                glosses_data.append([this_file, gloss_id, lemma_id, gloss_text])
+        if normalise:
+            glosses_data = [[gd[0], gd[1], gd[2], gd[3].lower()] for gd in glosses_data]
+            glosses_data = [[gd[0], gd[1], gd[2], "u".join(gd[3].split("v"))] for gd in glosses_data]
+            glosses_data = [[gd[0], gd[1], gd[2], "i".join(gd[3].split("j"))] for gd in glosses_data]
+            for punct in [p for p in string.punctuation + "«»"]:
+                glosses_data = [[gd[0], gd[1], gd[2], "".join(gd[3].split(punct))] for gd in glosses_data]
+            glosses_data = [[gd[0], gd[1], gd[2], " ".join(gd[3].split("  "))] for gd in glosses_data]
 
-    return prepped
+        # Pair glosses on the same lemma from different manuscripts, and remove all unpaired glosses
+        gloss_pairs = list()
+        for gl_index, gloss_datum in enumerate(glosses_data):
+            for remaining_gloss in glosses_data[gl_index:]:
+                if gloss_datum[0] != remaining_gloss[0] and gloss_datum[2] == remaining_gloss[2]:
+                    gloss_pairs.append([gloss_datum, remaining_gloss])
+
+        texts_dict[text] = gloss_pairs
+
+    return texts_dict
 
 
-def apply_bestmod(folder_path="default", model="default", cutoff="default", llm="default", output_filename="default",
+def apply_bestmod(model="default", cutoff="default", llm="default", output_filename="default",
                   include_false=True, normalise=False):
     """Uses the best performing model to perform text similarity analysis on glosses"""
 
-    full_gloss_pairs = prep_files(folder_path, normalise=normalise)
-    basic_gloss_pairs = [[i[0][3], i[1][3]] for i in full_gloss_pairs]
+    base_text_dict = prep_files(normalise=normalise)
+    base_texts = [i for i in base_text_dict if base_text_dict.get(i)]
 
-    if model == "default":
-        model = "LLM"
+    output_dir = os.path.join(os.getcwd(), "similarity_models", "models_output")
 
-    results = []
+    for base_text in base_texts:
+        full_gloss_pairs = base_text_dict.get(base_text)
+        basic_gloss_pairs = [[i[0][3], i[1][3]] for i in full_gloss_pairs]
 
-    if model == "ED":
-        if cutoff == "default":
-            cutoff = 60
-        results = [ed_compare(g[0], g[1], cutoff) for g in basic_gloss_pairs]
+        if model == "default":
+            model = "LLM"
 
-    elif model == "LCS":
-        if cutoff == "default":
-            cutoff = 82
-        results = [lcs_compare(g[0], g[1], cutoff) for g in basic_gloss_pairs]
+        results = []
 
-    elif model == "LLM":
+        if model == "ED":
+            if cutoff == "default":
+                cutoff = 60
+            results = [ed_compare(g[0], g[1], cutoff) for g in basic_gloss_pairs]
 
-        if cutoff == "default":
-            cutoff = 55
-        if llm == "default":
-            llm = "silencesys/paraphrase-xlm-r-multilingual-v1-fine-tuned-for-latin"
+        elif model == "LCS":
+            if cutoff == "default":
+                cutoff = 82
+            results = [lcs_compare(g[0], g[1], cutoff) for g in basic_gloss_pairs]
 
-        glosses_to_embed = sorted(list(set(
-            [g[0] for g in basic_gloss_pairs] + [g[1] for g in basic_gloss_pairs]
-        )))
+        elif model == "LLM":
 
-        llm = SentenceTransformer(llm)
-        embedded_glosses = llm.encode(glosses_to_embed)
+            if cutoff == "default":
+                cutoff = 55
+            if llm == "default":
+                model_used = "silencesys/paraphrase-xlm-r-multilingual-v1-fine-tuned-for-latin"
+            else:
+                model_used = llm
 
-        gloss_dict = dict()
-        for gloss_index, gloss in enumerate(glosses_to_embed):
-            gloss_dict[gloss] = embedded_glosses[gloss_index]
+            glosses_to_embed = sorted(list(set(
+                [g[0] for g in basic_gloss_pairs] + [g[1] for g in basic_gloss_pairs]
+            )))
 
-        results = [llm_compare(g[0], g[1], gloss_dict, llm, cutoff) for g in basic_gloss_pairs]
+            model_used = SentenceTransformer(model_used)
+            embedded_glosses = model_used.encode(glosses_to_embed)
 
-    related_glosses = list()
-    for result_index, result in enumerate(results):
-        if result == "Related":
-            related_glosses.append(full_gloss_pairs[result_index])
-    related_glosses = [
-        [pair[0][0], pair[0][1], pair[0][3], pair[1][0], pair[1][1], pair[1][3], "Related"] for pair in related_glosses
-    ]
+            gloss_dict = dict()
+            for gloss_index, gloss in enumerate(glosses_to_embed):
+                gloss_dict[gloss] = embedded_glosses[gloss_index]
 
-    output_glosses = related_glosses
+            results = [llm_compare(g[0], g[1], gloss_dict, model_used, cutoff) for g in basic_gloss_pairs]
 
-    if include_false:
-        unrelated_glosses = list()
+        related_glosses = list()
         for result_index, result in enumerate(results):
-            if result == "Unrelated":
-                unrelated_glosses.append(full_gloss_pairs[result_index])
-        unrelated_glosses = [
+            if result == "Related":
+                related_glosses.append(full_gloss_pairs[result_index])
+        related_glosses = [
             [
-                pair[0][0], pair[0][1], pair[0][3], pair[1][0], pair[1][1], pair[1][3], "Unrelated"
-            ] for pair in unrelated_glosses
+                pair[0][1], pair[0][2], pair[0][3], pair[1][1], pair[1][2], pair[1][3], "Related"
+            ] for pair in related_glosses
         ]
-        output_glosses = related_glosses + unrelated_glosses
 
-    if output_filename == "default":
-        if llm == "default":
-            if normalise:
-                output_filename = f"Related Gloss Output ({model} - Text Normalised).xlsx"
+        output_glosses = related_glosses
+
+        if include_false:
+            unrelated_glosses = list()
+            for result_index, result in enumerate(results):
+                if result == "Unrelated":
+                    unrelated_glosses.append(full_gloss_pairs[result_index])
+            unrelated_glosses = [
+                [
+                    pair[0][0], pair[0][1], pair[0][3], pair[1][0], pair[1][1], pair[1][3], "Unrelated"
+                ] for pair in unrelated_glosses
+            ]
+            output_glosses = related_glosses + unrelated_glosses
+
+        if output_filename == "default":
+            if llm == "default":
+                if normalise:
+                    out_file = f"Paired Glossses for {base_text} ({model} - Text Normalised).xlsx"
+                else:
+                    out_file = f"Paired Glossses for {base_text} ({model}).xlsx"
             else:
-                output_filename = f"Related Gloss Output ({model}).xlsx"
+                fixed_llm = "_".join(llm.split("/"))
+                fixed_llm = "_".join(fixed_llm.split("\\"))
+                if normalise:
+                    out_file = f"Paired Glossses for {base_text} ({fixed_llm} - Text Normalised).xlsx"
+                else:
+                    out_file = f"Paired Glossses for {base_text} ({fixed_llm}).xlsx"
+        elif "*base_text_name*" in output_filename:
+            out_file = base_text.join(output_filename.split("*base_text_name*"))
+            out_file = out_file + ".xlsx"
         else:
-            fixed_llm = "_".join(llm.split("/"))
-            fixed_llm = "_".join(fixed_llm.split("\\"))
-            if normalise:
-                output_filename = f"Related Gloss Output ({fixed_llm} - Text Normalised).xlsx"
-            else:
-                output_filename = f"Related Gloss Output ({fixed_llm}).xlsx"
-    else:
-        output_filename = output_filename + ".xlsx"
+            out_file = output_filename + ".xlsx"
 
-    df = pd.DataFrame(
-        output_glosses, columns=[
-            "Gl. 1 MS", "Gl. 1 no.", "Gloss 1", "Gl. 2 MS", "Gl. 2 no.", "Gloss 2", "Predicted Relationship"
-        ]
-    )
-    df.to_excel(output_filename, index=False)
+        df = pd.DataFrame(
+            output_glosses, columns=[
+                "Gl. 1 MS", "Gl. 1 no.", "Gloss 1", "Gl. 2 MS", "Gl. 2 no.", "Gloss 2", "Predicted Relationship"
+            ]
+        )
+        df.to_excel(os.path.join(output_dir, out_file), index=False)
 
 
 def apply_allmods(include_false=True, normalise=False):
@@ -177,9 +205,9 @@ def apply_allmods(include_false=True, normalise=False):
                     hf_model = "default"
                     lat_mod = "default"
                 if normalise:
-                    out_file = f"Related Gloss Output ({model_type} - {hf_model} - Text Normalised)"
+                    out_file = f"Paired Glossses for *base_text_name* ({model_type} - {hf_model} - Text Normalised)"
                 else:
-                    out_file = f"Related Gloss Output ({model_type} - {hf_model})"
+                    out_file = f"Paired Glossses for *base_text_name* ({model_type} - {hf_model})"
                 apply_bestmod(model=model_type, llm=lat_mod, output_filename=out_file,
                               include_false=include_false, normalise=normalise)
         else:
